7c67f5ac9b1434af456b24b82c8a8178b16b3ee4
jatinchhabria97 <71765051+jatinchhabria97@users.noreply.github.com>
2021-07-27 18:46:37 +1000

    Upload of the JS folder

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+(function($){
+	$(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrolltotop').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrolltotop').fadeIn();
+			} else {
+				jQuery('.scrolltotop').fadeOut();
+			}
+		});
+
+
+		// menuarea js
+		
+		selectnav('nav');
+		
+		
+		
+	});
+
+
+	$("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+  $(e.target)
+    .prev()
+    .find("i:last-child")
+    .toggleClass("fa-minus fa-plus");
+});
+
+
+
+
+})(jQuery);
+
+
